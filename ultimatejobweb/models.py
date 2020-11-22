87dec2f464aeb56068aea284e8f25551726bb1bb
Omer Nuri <omernu88@gmail.com>
2020-11-22 13:33:32 +0200

    fixed errors ./ultimatejobweb/models.py:6:9: E117 over-indented ./ultimatejobweb/tests.py:1:1: E265 block comment should start with '# ' ./ultimatejobweb/views.py:3:1: E302 expected 2 blank lines, found 1 ./ultimatejobweb/admin.py:1:1: E265 block comment should start with '# ' ./ultimatejobweb/models.py:6:8: E111 indentation is not a multiple of four ./ultimatejobweb/models.py:6:8: E117 over-indented ./ultimatejobweb/models.py:7:8: E111 indentation is not a multiple of four ./ultimatejobweb/models.py:8:8: E111 indentation is not a multiple of four

--- ultimatejobweb/models.py
+++ ultimatejobweb/models.py
@@ -3,8 +3,8 @@ from django.utils import timezone
 
 
 class homepage(models.Model):
-        author = models.CharField(max_length=200)
-        text = models.TextField()
-        date = models.DateTimeField(default=timezone.now)
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
 
 # Create your models here.
