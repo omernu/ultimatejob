b4e1f91fa85fca82820e8c3754fef0d4e94ebc70
Bar Ben Naim <ben.nain.bar@gmail.com>
2020-11-26 13:27:39 +0200

    before sim

--- ultimatejobweb/admin.py
+++ ultimatejobweb/admin.py
@@ -1,3 +1,5 @@
-# from django.contrib import admin
-
+from django.contrib import admin
+from .models import user, company
 # Register your models here.
+admin.site.register(user)
+admin.site.register(company)
--- ultimatejobweb/models.py
+++ ultimatejobweb/models.py
@@ -2,9 +2,24 @@ from django.db import models
 from django.utils import timezone
 
 
-class homepage(models.Model):
-    author = models.CharField(max_length=200)
-    text = models.TextField()
+class user(models.Model):
+    emailAndUsername = models.EmailField(max_length=254)
+    password = models.CharField(max_length=32)
+    firstName = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    age = models.IntegerField()
     date = models.DateTimeField(default=timezone.now)
+    profession = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.emailAndusername
+
+
+class company(models.Model):
+    UrlCompany = models.URLField(max_length=200)
+    CompanyName = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.CompanyName
 
 # Create your models here.
