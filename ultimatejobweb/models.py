de175870fa963720205a264ae8cb4ba9a516ba53
Bar Ben Naim <ben.nain.bar@gmail.com>
2020-11-26 14:10:07 +0200

    django user auth

--- ultimatejobweb/models.py
+++ ultimatejobweb/models.py
@@ -1,18 +1,11 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class user(models.Model):
-    emailAndUsername = models.EmailField(max_length=254)
-    password = models.CharField(max_length=32)
-    firstName = models.CharField(max_length=128)
-    last_name = models.CharField(max_length=128)
-    age = models.IntegerField()
-    date = models.DateTimeField(default=timezone.now)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     profession = models.CharField(max_length=128)
-
-    def __str__(self):
-        return self.emailAndusername
 
 
 class company(models.Model):
