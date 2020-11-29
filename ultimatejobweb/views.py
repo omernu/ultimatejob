2427cfada9f6f2e26d2ba672689f0ff0e3f92836
Bar Ben Naim <ben.nain.bar@gmail.com>
2020-11-29 20:33:53 +0200

    create signup page
    a signup page that can creat users and conect to the DB

--- ultimatejobweb/views.py
+++ ultimatejobweb/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 
 
 def homepage(request):
     return render(request, r"web/ultimate_job_homepage.html")
 # Create your views here.
+
+
+def signup_view(request):
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+#login(request, user)
+        return redirect('homepage')
+    return render(request, r"web/signup.html", {'form': form})
