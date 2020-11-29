a02371235f432eb036a31dfcc53d04cf5cf3341a
Bar Ben Naim <ben.nain.bar@gmail.com>
2020-11-29 16:54:10 +0200

    after sim notes

--- ultimatejobweb/admin.py
+++ ultimatejobweb/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-from .models import user, company
 from django.apps import apps
 
 # Register your models here.
-admin.site.register(user)
-admin.site.register(company)
 models = apps.get_models()
 
 for model in models:
