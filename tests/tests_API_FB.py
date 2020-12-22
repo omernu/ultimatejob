95b4815cd2dcde74e562946e88ed95cff93a0fe5
Rom Tamam <rom.tamam@gmail.com>
2020-12-22 21:39:23 +0200

    fix flake

--- tests/tests_API_FB.py
+++ tests/tests_API_FB.py
@@ -1,39 +1,30 @@
 # Import and load necessary lib
-#import sys
-#sys.path.append('vagrant\ultimatejob')
+# import sys
+# sys.path.append('vagrant\ultimatejob')
 import API_FB
-import pytest
-
-
-def main():
-    # Create soup object & print a status message - 404 not good , 200 good
-    soup = API_FB.create_soup(API_FB.create_req())
-
-    # Return list contains URLs
-    API_FB.extract_jobs_url(soup)
-
-    # Return list contains job titles
-    API_FB.extract_jobs_title(soup)
 
 
 # Test functions
 def test_api_urls():
     """The testing will check if our functions return the required output"""
-    expected_output_urls = ['https://www.facebook.com/careers/jobs/1672813472870915/',
-                            'https://www.facebook.com/careers/jobs/712878282607325/',
-                            'https://www.facebook.com/careers/jobs/420212419368641/',
-                            'https://www.facebook.com/careers/jobs/207984144060781/',
-                            'https://www.facebook.com/careers/jobs/773785633436218/',
-                            'https://www.facebook.com/careers/jobs/2597607417128369/',
-                            'https://www.facebook.com/careers/jobs/323705805605018/',
-                            'https://www.facebook.com/careers/jobs/536078547072736/']
-    assert API_FB.extract_jobs_url() == expected_output_urls, "The tests for URLs were successful"
+    # Create soup object & print a status message - 404 not good , 200 good
+    soup = API_FB.create_soup(API_FB.create_req())
+    testval = 'testval'
+    expected_output_urls =  ['https://www.facebook.com/careers/v2/jobs/1672813472870915/',
+                             'https://www.facebook.com/careers/v2/jobs/712878282607325/',
+                             'https://www.facebook.com/careers/v2/jobs/420212419368641/',
+                             'https://www.facebook.com/careers/v2/jobs/207984144060781/',
+                             'https://www.facebook.com/careers/v2/jobs/773785633436218/',
+                             'https://www.facebook.com/careers/v2/jobs/2597607417128369/',
+                             'https://www.facebook.com/careers/v2/jobs/536078547072736/']
+    assert API_FB.extract_jobs_url(soup) == expected_output_urls, "The tests for URLs were successful"
 
 
 def test_api_titles():
+    # Create soup object & print a status message - 404 not good , 200 good
+    soup = API_FB.create_soup(API_FB.create_req())
     """The testing will check if our functions return the required output"""
     expected_output_titles = ['Production Engineer', 'Offensive Security Engineer Intern, Red Team',
                               'Internal Audit Manager – Infrastructure', 'Production Engineer',
-                              'Production Engineer', 'Return to Work - Production Engineer',
-                              'Production Engineer', 'Production Engineer']
-    assert API_FB.extract_jobs_title() == expected_output_titles, "The tests for URLs were successful"
+                              'Production Engineer', 'Production Engineer', 'Production Engineer']
+    assert API_FB.extract_jobs_title(soup) == expected_output_titles, "The tests for URLs were successful"
