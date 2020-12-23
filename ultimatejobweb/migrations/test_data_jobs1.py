f5b5ba4cab533f848b3e3847ceb03775d75a3ed7
Omer Nuri <omernu88@gmail.com>
2020-12-23 20:17:55 +0200

    show results

--- ultimatejobweb/migrations/test_data_jobs1.py
+++ ultimatejobweb/migrations/test_data_jobs1.py
@@ -1,5 +1,5 @@
 from django.db import migrations, transaction
-from ultimatejobweb.models import job, company
+from ultimatejobweb.models import Job, Company
 
 
 class Migration(migrations.Migration):
@@ -9,14 +9,14 @@ class Migration(migrations.Migration):
     ]
 
     def generate_data(apps, schema_editor):
-        company1 = company(1, '"https://careers-redhat.icims.com/jobs/search?ss=1&in_iframe=1&searchKeyword=',
+        company1 = Company(1, '"https://careers-redhat.icims.com/jobs/search?ss=1&in_iframe=1&searchKeyword=',
                            'logo1', 'function1')
-        company2 = company(2, 'https://www.amazon.jobs/en/', 'logo2', 'function2')
+        company2 = Company(2, 'https://www.amazon.jobs/en/', 'logo2', 'function2')
 
         jobs = [
-          (company1, 'DevOps', 'Production Engineer',
+          (company1, 'Production Engineer',
            'https://www.facebook.com/careers/jobs/1672813472870915/'),
-          (company2, 'DevOps', 'Production Engineer',
+          (company2, 'Production Engineer',
            'https://www.facebook.com/careers/jobs/712878282607325/'),
         ]
 
@@ -27,13 +27,12 @@ class Migration(migrations.Migration):
         ]
 
         with transaction.atomic():
-            for CompanyName, CompanySearchUrl, CompanyLogo, FunctionName in companies:
-                company(CompanyName=CompanyName, CompanySearchUrl=CompanySearchUrl, CompanyLogo=CompanyLogo,
-                        FunctionName=FunctionName).save()
+            for company_name, company_search_url, company_logo, function_name in companies:
+                Company(company_name=company_name, company_search_url=company_search_url, company_logo=company_logo,
+                        function_name=function_name).save()
 
-            for CompanyName, SearchKey, JobTitle, DescriptionURL in jobs:
-                job(CompanyName=CompanyName, SearchKey=SearchKey, JobTitle=JobTitle,
-                    DescriptionURL=DescriptionURL).save()
+            for company, job_title, description_url in jobs:
+                Job(company=company, job_title=job_title, description_url=description_url).save()
 
     operations = [
         migrations.RunPython(generate_data),
