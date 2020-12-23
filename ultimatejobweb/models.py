d73f669b03d0be3e0810ba582fa1ca7349108087
Bar Ben Naim <ben.nain.bar@gmail.com>
2020-12-23 20:09:57 +0200

    after sim change

--- ultimatejobweb/models.py
+++ ultimatejobweb/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 class company(models.Model):
-    CompanyName = models.CharField(max_length=128)
-    CompanySearchUrl = models.URLField()
-    CompanyLogo = models.TextField()
-    FunctionName = models.TextField()
+    company_name = models.CharField(max_length=128)
+    company_search_url = models.URLField()
+    company_logo = models.TextField()
+    function_name = models.TextField()
 
 
 class job(models.Model):
-    CompanyName = models.ForeignKey(company, on_delete=models.CASCADE)
-    SearchKey = models.CharField(max_length=128)
-    JobTitle = models.CharField(max_length=128)
-    DescriptionURL = models.URLField()
+    company = models.ForeignKey(company, on_delete=models.CASCADE)
+    search_key = models.CharField(max_length=128)
+    job_title = models.CharField(max_length=128)
+    description_url = models.URLField()
 
 # Create your models here.
