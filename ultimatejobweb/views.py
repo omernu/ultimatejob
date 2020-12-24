355719456294c250057133ad9fca3721a44da4c7
Erez David Laor <erezdav22@gmail.com>
2020-12-24 18:24:19 +0200

    addind / to clode footer tag

--- ultimatejobweb/views.py
+++ ultimatejobweb/views.py
@@ -1,19 +1,11 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import redirect
 from .forms import SignUpFroms
 
 
 def homepage(request):
     return render(request, r"home_page.html")
-
-
-def signup_view(request):
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-        form.save()
-        return redirect('homepage')
-    return render(request, r"sign_up.html", {'form': form})
 
 
 def sign_up(request):
@@ -24,12 +16,18 @@ def sign_up(request):
     return render(request, r"sign_up.html", {'form': form})
 
 
+def sign_in_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            return redirect('personal_area')
+    else:
+        form = AuthenticationForm()
+    return render(request, r"sign_in.html", {'form': form})
+
+
 def personal_area(request):
     return render(request, r"personal_area.html")
-
-
-def sign_in(request):
-    return render(request, r"sign_in.html")
 
 
 def available_jobs(request):
