7b12824d63f97cb82655e9b915991434085ea848
Omer Nuri <omernu88@gmail.com>
2020-12-26 17:23:58 +0200

    result page

--- ultimatejobweb/views.py
+++ ultimatejobweb/views.py
@@ -34,4 +34,5 @@ def sign_in(request):
 
 
 def available_jobs(request):
-    return render(request, r"available_jobs.html", {'job': Job.objects.all()})
+    return render(request, r"available_jobs.html", {'job': Job.objects.all()}) 
+
