e90393ce4d2b17f09ecceafd6a70dbe708884b22
Omer Nuri <omernu88@gmail.com>
2020-12-27 10:55:43 +0200

    now display the company name

--- ultimatejobweb/models.py
+++ ultimatejobweb/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Company(models.Model):
-
     company_name = models.CharField(max_length=128)
     company_search_url = models.URLField()
     company_logo = models.TextField()
@@ -13,6 +12,5 @@ class Job(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     job_title = models.CharField(max_length=128)
     description_url = models.URLField()
-
 
 # Create your models here.
