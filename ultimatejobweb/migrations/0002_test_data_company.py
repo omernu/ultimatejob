f2370604634bae01d3bd2dde760ed40a078a0814
Bar Ben Naim <ben.nain.bar@gmail.com>
2020-12-28 19:17:35 +0200

    after flake8

--- ultimatejobweb/migrations/0002_test_data_company.py
+++ ultimatejobweb/migrations/0002_test_data_company.py
@@ -1,21 +1,21 @@
 from django.db import migrations, transaction
 from ultimatejobweb.models import company
 
+
 class Migration(migrations.Migration):
 
     dependencies = [('ultimatejobweb', '0001_initial')]
 
-    def generate_data (apps, schema_editor):
+    def generate_data(apps, schema_editor):
 
         companies = [('Facebook', 'https://www.facebook.com/careers/jobs?q=',
                       'templates/company logo/FBlogo.png', 'manipuletion_facebook'),
                      ('Amazon', 'https://www.amazon.jobs/en/search?base_query=&loc_query=',
                       'templates/company logo/AMAZONlogo.png', 'manipuletion_amazon')]
 
-
         with transaction.atomic():
             for company_name, company_search_url, company_logo, function_name in companies:
                 company(company_name=company_name, company_search_url=company_search_url, company_logo=company_logo,
                         function_name=function_name).save()
 
-    operations = [migrations.RunPython(generate_data),]
+    operations = [migrations.RunPython(generate_data), ]
--- ultimatejobweb/migrations/0003_test_data_job.py
+++ ultimatejobweb/migrations/0003_test_data_job.py
@@ -8,12 +8,12 @@ class Migration(migrations.Migration):
         ('ultimatejobweb', '0002_test_data_company'),
     ]
 
-    def generate_data(apps, schema_editor):      
-        jobs = [('Production Engineer','https://www.facebook.com/careers/jobs/1672813472870915/'),
-                ('Product M','https://www.facebook.com/careers/jobs/16732472870915/')]
-        
+    def generate_data(apps, schema_editor):
+        jobs = [('Production Engineer', 'https://www.facebook.com/careers/jobs/1672813472870915/'),
+                ('Product M', 'https://www.facebook.com/careers/jobs/16732472870915/')]
+
         with transaction.atomic():
-            n=0
+            n = 0
             for job_title, description_url in jobs:
                 n += 1
                 company_n = company.objects.get(id=n)
